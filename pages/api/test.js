72c02f75ea7cc52d960031fea706bbc150983515
John.Huang <john.huang@onelab.tw>
2020-07-24 10:14:10 +0800

    add test api route

---
+++ pages/api/test.js
@@ -0,0 +1,7 @@
+export default (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        message: 'Hello from Test Vendor'
+    }));
+}
