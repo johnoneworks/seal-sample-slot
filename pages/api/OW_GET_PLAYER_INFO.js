1ee1d909c3eed294a4023042b7efc9174ca656d8
John.Huang <john.huang@onelab.tw>
2020-07-28 10:48:14 +0800

    working getPlayerInfo api on game 1

---
+++ pages/api/OW_GET_PLAYER_INFO.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import nextConnect from 'next-connect';
+import moment from 'moment';
+
+const handler = nextConnect();
+
+//handler.use(middleware);
+
+handler.post(async (req, res) => {
+    console.log("get player info api");
+    console.log(req.body);
+    let data = {
+        "Timestamp": req.body.Timestamp,
+        "Seq": req.body.Seq,
+        "Token": req.body.Token,
+        "CustId": req.body.CustId,
+        "GameCode": req.body.GameCode
+    };
+    let config = {
+        method: 'post',
+        url: 'http://owapi1.playthefun.com:2111/api/SlotGameDemo/GetPlayerInfo',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    let response = await axios(config);
+    console.log("response");
+    console.log(response.data);
+    res.json(response.data);
+});
+
+export default handler;
--- pages/game-1/[ow]/game-1.js
+++ pages/game-1/[ow]/game-1.js
@@ -1,16 +1,70 @@
-import { Component, useState } from "react";
-import { useRouter } from 'next/router'
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import moment from 'moment';
 
 import Layout from '../../../components/Layout';
-import Error from '../../_error';
+
+const MODE = 1; // 0: mock mode, 1: actual OW API call
 
 
 const Game1 = () => {
     const router = useRouter();
     const { CustId, Token } = router.query;
 
-    let [username, setUsername] = useState("John");
     let [balance, setBalance] = useState(200);
+    let [playerInfo, setPlayerInfo] = useState({});
+    useEffect(() => {
+        async function getPlayerInfoWrapper() {
+            switch (MODE) {
+                case 0: // mock the return data from OW
+                    setPlayerInfo({
+                        CustId: 17854493,
+                        MinBet: 10,
+                        MaxBet: 2500,
+                        CurrencyId: 16,
+                        SiteId: 4100200,
+                        UserName: "twpgmsobichen"
+                    });
+                    setBalance(3000);
+                    break;
+                case 1:
+                    let timeStamp = moment().toJSON();
+                    let token = router.query.Token;
+                    let custId = router.query.CustId;
+                    console.log(timeStamp);
+                    console.log(token);
+                    console.log(custId);
+                    let data = {
+                        "Timestamp": timeStamp,
+                        "Token": token,
+                        "CustId": custId,
+                        "Seq": "test3-test4-test5-test8",
+                        "GameCode": "SAMPLE_GAME_1"
+                    };
+                    let config = {
+                        method: 'post',
+                        url: '/api/OW_GET_PLAYER_INFO',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        data: data
+                    };
+
+                    let response = await axios(config);
+                    console.log("response");
+                    console.log(response.data);
+                    setPlayerInfo(response.data);
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        getPlayerInfoWrapper();
+    }, [CustId, Token]);
+
+
     let [slot1, setSlot1] = useState("?");
     let [slot2, setSlot2] = useState("?");
     let [slot3, setSlot3] = useState("?");
@@ -33,14 +87,51 @@ const Game1 = () => {
         <Layout title="Game 1">
             <p>A Sample Slot Game</p>
             <img src="/static/game1-logo.jpg" alt="Game logo" height="200px" />
+            <ul>
+                <li>
+                    QueryParams:
+                        <ul>
+                        <li>
+                            CustId - {CustId ? CustId : "null"}
+                        </li>
+                        <li>
+                            Token - {Token ? Token : "null"}
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    API Response:
+                    <ul>
+                        <li>
+                            Timestamp - {playerInfo.Timestamp}
+                        </li>
+                        <li>
+                            Seq - {playerInfo.Seq}
+                        </li>
+                        <li>
+                            ErrorCode - {playerInfo.ErrorCode}
+                        </li>
+                        <li>
+                            ErrorDescription - {playerInfo.ErrorDescription}
+                        </li>
+                        <li>
+                            MinBet - {playerInfo.MinBet}
+                        </li>
+                        <li>
+                            MaxBet - {playerInfo.MaxBet}
+                        </li>
+                        <li>
+                            SiteId - {playerInfo.SiteId}
+                        </li>
+                        <li>
+                            Username - {playerInfo.UserName}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
             <br />
-            <h2>
-                QueryParams: CustId - {CustId}, Token - {Token}
-                <br />
-                Username: {username}
-                <br />
-                Remaining Balance: {balance} (all bets are 10 for now)
-            </h2>
+
             <button onClick={handleSpin}>
                 SPIN
             </button>
@@ -62,69 +153,6 @@ const Game1 = () => {
             }
         </Layout>
     );
-}
-
-class Game1Old extends Component {
-
-    state = {
-        username: "John",
-        balance: 200,
-        slot1: "?",
-        slot2: "?",
-        slot3: "?"
-    }
-
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-
-    handleSpin = () => {
-        let slot1 = this.getRandomInt(0, 2);
-        let slot2 = this.getRandomInt(0, 2);
-        let slot3 = this.getRandomInt(0, 2);
-        this.setState({
-            slot1: (slot1 % 2 === 0) ? "X" : "O",
-            slot2: (slot2 % 2 === 0) ? "X" : "O",
-            slot3: (slot3 % 2 === 0) ? "X" : "O",
-            balance: this.state.balance - 10,
-        });
-    }
-
-    render() {
-        return (
-            <Layout title="Game 1">
-                <p>A Sample Slot Game</p>
-                <img src="/static/game1-logo.jpg" alt="Game logo" height="200px" />
-                <br />
-                <h2>
-                    Username: {this.state.username}
-                    <br />
-                    Remaining Balance: {this.state.balance} (all bets are 10 for now)
-                </h2>
-                <button onClick={this.handleSpin}>
-                    SPIN
-                </button>
-                <br />
-                <h3>
-                    {this.state.slot1} - {this.state.slot2} - {this.state.slot3}
-                </h3>
-                {// Jackpot
-                    (this.state.slot1 === this.state.slot2 && this.state.slot2 === this.state.slot3 && this.state.slot3 === "O") &&
-                    <span>JACKPOT!</span>
-                }
-                {// Regular Winner
-                    (this.state.slot1 === this.state.slot2 && this.state.slot2 === this.state.slot3 && this.state.slot3 === "X") &&
-                    <span>Regular Winner</span>
-                }
-                {// Loser...
-                    (!(this.state.slot1 === this.state.slot2 && this.state.slot2 === this.state.slot3 && this.state.slot3 === "O") && !(this.state.slot1 === this.state.slot2 && this.state.slot2 === this.state.slot3 && this.state.slot3 === "X")) &&
-                    <span>Try Again</span>
-                }
-            </Layout>
-        );
-    }
 }
 
 export default Game1;
